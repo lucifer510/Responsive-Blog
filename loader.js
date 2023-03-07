e1d7335612017b3d7a6c1f21f6ce8d4bf6a9c92b
Mihir Prajapati <prajapatimihir799@gmail.com>
2023-03-07 09:30:55 +0530

    modified:   about.html 	new file:   assets/loader.gif 	modified:   blogpost.html 	modified:   contact.html 	modified:   css/style.css 	modified:   index.html 	new file:   loader.js 	modified:   search.html

---
+++ loader.js
@@ -0,0 +1,5 @@
+// Description: This file is used to show the loader when the page is loading
+const loader = document.getElementById("preLoader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
